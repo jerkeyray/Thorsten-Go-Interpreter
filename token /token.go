28aada4aede823cb54d048af6117e99326183e06
srivastavya02 <srivastavya24@gmail.com>
2025-04-12 18:28:36 +0530

    setup token structure

---
+++ token /token.go
@@ -0,0 +1,35 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	//Identifiers and Literals
+	IDENT = "IDENT"
+	INT = "INT"
+
+	//OPERATORS 
+	ASSIGN = "="
+	PLUS = "+"
+
+	//DELIMITERS
+	COMMA = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	//KEYWORDS
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+
+)
